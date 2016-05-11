fdec000f2ec3e7f9db4c3afb4abcb8ddfb8bc7c2
Reade <Reade@DN0a226260.SUNet>
2016-05-11 09:41:31 -0700

    updated 9am 5/11

--- 3-charts/scatter.py
+++ 3-charts/scatter.py
@@ -12,14 +12,12 @@ Y = df['Followers']
 color = 'seismic'
 fig, ax = plt.subplots()
 
-ax.scatter(X, Y, cmap=color, alpha=0.5);
+ax.scatter(X, Y, cmap=color, alpha=0.5)
+ax.set_ylim(0, 1000000) # Remove some outliers (in this case, Obama) for legibility (thanks Alec for the tip!)
 
 ax.legend()
 ax.set_title("US Legislators:\nIs there a correlation between the number of times they tweet and followers?", fontsize=16)
 ax.set_ylabel("Number of followers", fontsize=14)
 ax.set_xlabel("Number of tweets", fontsize=14)
-
-plt.colorbar(color)
-plt.show()
 
 plt.savefig("tweets-v-followers.png", bbox_inches="tight") 
--- week-06/inclass_notes.py
+++ week-06/inclass_notes.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 
-
 # takes two sequences
 
 xvals = [0, 1, 2, 3]
@@ -17,3 +16,28 @@ ply.close()
 # for examples...
 fig, ax = plt.subplots()
 ax.pie([12,32])
+
+axs.bar(yrs, means)
+
+~~~py
+hots = []
+colds = []
+
+for d in datarows:
+	year = int(d["year"])
+	mean = float(d["annual_mean"])
+	if mean <= 0:
+		colds.append([year, mean])
+	else:
+		hots.appent([year, mean])
+
+fig, ax = plt.subplots()
+
+
+# ax.bar() ???
+
+
+# PANDAS!
+
+import pandas as pd
+
